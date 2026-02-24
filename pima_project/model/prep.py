80f231d8f394e728ebc449356823c5da91f33532
sandeep-raghuwanshi28 <sraghuwanshi28@gmail.com>
2026-02-24 16:25:10 +0530

    fix file not foundbug  and prep.py

--- pima_project/model/prep.py
+++ pima_project/model/prep.py
@@ -5,15 +5,15 @@ import os
 # for data preprocessing and pipeline creation
 from sklearn.model_selection import train_test_split
 # for hugging face space authentication to upload files
-from huggingface_hub import login, HfApi
-
+from huggingface_hub import HfApi
 
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 # Initialize API client
 api = HfApi(token=HF_TOKEN)
 
-DATASET_PATH = "hf://datasets/Diabetes-Prediction/diabetes.csv"
+# Load dataset from the correct path
+DATASET_PATH = "hf://datasets/sandeep-raghuwanshi28/Diabetes-Prediction/diabetes.csv"
 data = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
 
@@ -46,21 +46,21 @@ y = data[target_col]
 
 # Perform train-test split
 Xtrain, Xtest, ytrain, ytest = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-Xtrain.to_csv("Xtrain.csv",index=False)
-Xtest.to_csv("Xtest.csv",index=False)
-ytrain.to_csv("ytrain.csv",index=False)
-ytest.to_csv("ytest.csv",index=False)
+Xtrain.to_csv("Xtrain.csv", index=False)
+Xtest.to_csv("Xtest.csv", index=False)
+ytrain.to_csv("ytrain.csv", index=False)
+ytest.to_csv("ytest.csv", index=False)
 
-
-files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+files = ["Xtrain.csv", "Xtest.csv", "ytrain.csv", "ytest.csv"]
 
 for file_path in files:
     api.upload_file(
         path_or_fileobj=file_path,
-        path_in_repo=file_path.split("/")[-1],  # just the filename
+        path_in_repo=file_path,
         repo_id="sandeep-raghuwanshi28/Diabetes-Prediction",
         repo_type="dataset",
     )
+    print(f"Uploaded {file_path}")
